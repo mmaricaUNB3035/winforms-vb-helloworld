819b5fc2a03d24144590e74f7425063f93cbdce8
Matei Marica <matei.marica@outlook.com>
2023-11-16 12:04:31 -0400

    init commit

---
+++ CS3035_Presentation_VB_WinForms_Tests/HelloWorldForm.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Windows.Forms
+
+Public Class HelloWorldForm
+    Inherits Form
+
+    Private counter = 0
+    Private counterLabel
+
+    Sub New() ' constructor
+
+        Dim helloLabel As New Label()
+        helloLabel.Text = "Hello, world!"
+        helloLabel.TextAlign = ContentAlignment.MiddleCenter
+        helloLabel.Margin = New Padding(
+            (Me.ClientSize.Width - helloLabel.Width) / 2,
+            80,
+            helloLabel.Margin.Right,
+            helloLabel.Margin.Bottom
+        )
+
+        Dim incrementButton As New Button
+        incrementButton.Text = "Increment"
+        AddHandler incrementButton.Click, AddressOf IncrementButton_Click
+
+        Dim counterLabel As New Label
+        counterLabel.TextAlign = ContentAlignment.MiddleCenter
+        counterLabel.Text = counter
+        Me.counterLabel = counterLabel
+
+        Dim subPanel As New FlowLayoutPanel
+        subPanel.Controls.AddRange({incrementButton, counterLabel})
+        subPanel.FlowDirection = FlowDirection.LeftToRight
+        subPanel.Margin = New Padding(
+            (Me.ClientSize.Width - incrementButton.Width - counterLabel.Width) / 2,
+            subPanel.Margin.Top,
+            subPanel.Margin.Right,
+            subPanel.Margin.Bottom
+        )
+
+        Dim mainPanel As New FlowLayoutPanel
+        mainPanel.Controls.AddRange({helloLabel, subPanel})
+        mainPanel.FlowDirection = FlowDirection.TopDown
+        mainPanel.Dock = DockStyle.Fill
+
+        Me.Controls.Add(mainPanel)
+
+    End Sub
+
+    Private Sub IncrementButton_Click()
+        counter += 1
+        counterLabel.Text = counter
+    End Sub
+
+    Shared Sub Main() ' entry point
+        Application.Run(New HelloWorldForm())
+    End Sub
+End Class
---
+++ CS3035_Presentation_VB_WinForms_Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CS3035_Presentation_VB_WinForms_Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CS3035_Presentation_VB_WinForms_Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c8b2e661-7c48-4657-b630-87293924be8f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
